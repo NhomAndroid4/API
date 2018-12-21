ba74ff6ceee18f7ab442993cbfda18586d924132
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2018-12-21 22:02:03 +0700

    DucPham

---
+++ app.js
@@ -0,0 +1,38 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var routes = require("./routes/routes.js");
+var app = express();
+//console.log('...MONGO EVENT ERROR');
+//----Kết nối Mongodb bằng mongoose
+var mongodb_url = 'mongodb://localhost:27017/DucPham';
+mongoose.Promise = global.Promise;
+var MongoOptions = {
+    poolSize: 10,
+    reconnectTries: 3600,
+    reconnectInterval: 1000,
+    autoReconnect: true,
+    useNewUrlParser: true
+};
+mongoose.set('useCreateIndex', true);
+mongoose.connect(mongodb_url, MongoOptions);
+var db = mongoose.connection;
+db.on('error', function (err) {
+    console.log('Ket noi MongoDB loi');
+});
+db.on('connected', function () {
+    console.log('Ket noi MongoDB thanh cong');
+    console.log('Dang ket noi Database: ' + db.name);
+	console.log('--------------------------------------------');
+});
+//---------------
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
+
+var server = app.listen(3000, function () {
+    console.log("Application running on port: ", server.address().port);
+});
---
+++ routes/routes.js
@@ -0,0 +1,79 @@
+﻿var users = require('../models/users');
+
+var appRouter = function (app) {
+
+  app.get("/", function (req, res) {
+    res.status(200).send("Welcome to WebService By DucPham");
+  });
+
+    app.post("/DangNhap", function (req, res) {
+        //console.log(req.body);
+        var data = {
+            username: req.body.username,
+            password: req.body.password,
+        }
+        //var user = req.body.username;
+        //var pass = req.body.password;
+        //console.log('Tai khoan: ' + user);
+        //console.log('Mat khau: ' + pass);
+        //var bFound = false;
+        //var key = '';
+        //if (user === 'vvdung' && pass === '123456') {
+        //   key = JSON.stringify({user:user});
+        //    bFound = true;
+        //}
+
+        //var dateRet = { r: bFound, key: key };
+
+        //res.status(200).send(dateRet);
+
+		users.DangNhap(data, function(ret){
+			console.log('DangNhap() => ' + ret);
+			if (ret == 1)
+                res.status(200).send("Dang nhap thanh cong");
+            else
+                res.status(200).send("Dang nhap that bai :" + ret);
+		});
+    });
+
+    app.post("/DangKy", function (req, res) {
+        var data = {
+            uname: req.body.uname,
+            pass: req.body.pass,
+            email: req.body.email
+        }
+
+        users.AddUser(data, function (ret) {
+            console.log('AddUser() => ' + ret);
+            if (ret == 1)
+                res.status(200).send("Dang ky thanh cong");
+            else
+                res.status(200).send("Dang ky that bai :" + ret);
+        });
+    });
+
+    app.post("/getinfo", function (req, res) {
+        //1.Nhan tham so tu client
+        var key = req.headers['token'];
+        if (!key) {
+            res.status(302).send("CHUA DANG NHAP");
+            return;
+        }
+
+        //console.log(req.body);
+        //console.log(' TEN TAI KHOAN: ' + req.body.username);
+        //2. xu ly, truy xuat du lieu.......
+
+        //3. Gui ket qua theo cau truc JSON
+        var ret = JSON.stringify("{r:'API GETINFO - NODEJS - TINK39', key2: 'value 2', key3: true}");
+        res.status(200).send(ret);
+    });
+
+    app.get("/Get", function (req, res) {
+      users.Get(function(ret){
+        console.log('GET() => ' + ret);
+      });
+    });
+}
+
+module.exports = appRouter;
